f1070be6bc22dce791ea15ae2540ccece9b543be
Marco Filgueiras <marco.filgueiras@hotmail.com>
2020-07-29 19:26:17 -0300

    Verificar se compila o repositório.

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/DefaultPage';
 
+
 function CadastroCategoria() {
+
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Dummy');
+
+
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria} </h1>
 
       <form>
 
@@ -13,6 +19,14 @@ function CadastroCategoria() {
           Nome da Categoria:
           <input
             type="text"
+            defaultValue={nomeDaCategoria}
+            onChange={
+              function funcaoHandler(dadosEvento) {
+                //console.log('[nomeDaCategoria]', nomeDaCategoria);
+                //console.log('[dadosEvento.target]', dadosEvento.target.value);
+                setNomeDaCategoria(dadosEvento.target.value);
+              }
+            } 
           />
         </label>
 
